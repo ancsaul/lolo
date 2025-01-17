5b07f5f8843a1e8182820a408dcbea56b79acd5d
ancsaul <ancona.saul01@gmail.com>
2025-01-17 11:34:17 -0600

    matadero

--- poo_con_python.py
+++ poo_con_python.py
@@ -1,83 +1,117 @@
-class personaje: 
-    #atributos de la clase
-    # nombre ='default'
-    # fuerza = 0 
-    # inteligencia = 0
-    # vida = 0
-   
-
-
-    def __init__ (self, nombre, fuerza, inteligencia, vida, defensa):
+class Personaje:
+    # Constructor de la clase
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
         self.nombre = nombre
         self.fuerza = fuerza
-        self.vida = vida
         self.inteligencia = inteligencia
         self.defensa = defensa
-
-
-#¿que rayos significa self? referencia al mismo aobjeto
-#¿que es init? constructor que inicializa el atributo del objeto
-#¿por que tiene doble __ en el init? porque es un metodo magico (dunder)
-#¿en que momento se ejecuta el momento init en poo python? cuando se crea un objeto
+        self.vida = vida
 
     def imprimir_atributos(self):
         print(self.nombre)
-        print("-fuerza:", self.fuerza)
-        print("-inteligencia:", self.inteligencia)
-        print("-vida:", self.vida)
-        print("-defensa:", self.defensa)
-
-
-    def subir_nivel(self, fuerza, inteligencia):
-        self.fuerza = self.fuerza + fuerza
-        self.inteligencia = self.inteligencia + inteligencia
-
+        print("-Fuerza:", self.fuerza)
+        print("-Inteligencia:", self.inteligencia)
+        print("-Defensa:", self.defensa)
+        print("-Vida:", self.vida)
+    
+    def subir_nivel(self, fuerza, inteligencia, defensa):
+        self.fuerza += fuerza   
+        self.inteligencia += inteligencia
+        self.defensa += defensa
+    
     def esta_vivo(self):
         return self.vida > 0
-
+    
     def morir(self):
         self.vida = 0
-        print(self.nombre, " ha muerto")
-       # return self.vida <= 0
-
-    def dañar(self, enemigo):
-        return self.fuerza - enemigo.defensa
-
-
+        print(self.nombre, "Ha muerto")
+        return self.vida <= 0
+    
+    def dmg(self, enemigo):
+        # Si la defensa del enemigo es mayor o igual que la fuerza del atacante, el daño es 0
+        return max(0, self.fuerza - enemigo.defensa)
+    
     def atacar(self, enemigo):
-        daño = self.dañar(enemigo)
-        enemigo.vida = enemigo.vida - daño
-        print(self.nombre, " ha ralizado", daño, "puntos de daño a", enemigo.nombre)
-        print("vida de ", enemigo.nombre, "es", enemigo.vida)
+        daño = self.dmg(enemigo)
+        # Asegurarse de que la vida no baje de cero
+        enemigo.vida = max(0, enemigo.vida - daño)
+        print(self.nombre, "Ha realizado", daño, "puntos de daño a", enemigo.nombre)
+        print("Vida de", enemigo.nombre, "es", enemigo.vida)
 
-class guerrero(personaje):
-    pass 
+class Guerrero (Personaje):
+    #Sobrescribir constructor
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, espada):
+        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+        self.espada = espada 
 
-tlatoani = guerrero("apocalipto", 50, 70, 30, 100) 
+    #Sobrescribir impresion
+    def imprimir_atributos(self):
+        super().imprimir_atributos()
+        print("-Espada:", self.espada)
+        
+    def elegir_arma(self):
+        opcion = int(input("Elige un arma: \n(1)lanza de obsidiana, dmg 10 \n(2)Lanza de chaya, dmg 6\n<<"))
+        if opcion == 1:
+            self.espada = 10
+        elif opcion == 2:
+            self.espada == 5
+        else:
+            print("Opcion no valida")
+            self.elegir_arma()
+    
+    #Sobrescribir calculo de dmg
+    def dmg(self, enemigo):
+        return self.fuerza*self.espada - enemigo.defensa
+ 
+class Mago (Personaje):
+    #Sobrescribir constructor
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, libro):
+        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+        self.libro = libro 
 
+    #Sobrescribir impresion
+    def imprimir_atributos(self):
+        super().imprimir_atributos()
+        print("-Libro:", self.libro)
+        
+    def elegir_arma(self):
+        opcion = int(input("Elige un arma: \n(1)Hechizoz de programacion, dmg 10 \n(2)Recetario de chaya, dmg 6\n<<"))
+        if opcion == 1:
+            self.libro = 10
+        elif opcion == 2:
+            self.libro == 5
+        else:
+            print("Opcion no valida")
+            self.elegir_arma()
+    
+    #Sobrescribir calculo de dmg
+    def dmg(self, enemigo):
+        return self.inteligencia*self.libro - enemigo.defensa
 
-#variable del constructor vacio de la clse
-# mi_personaje = personaje("dante", 100, 3, 100, 100)
-# mi_personaje.imprimir_atributos()
-# mi_personaje.subir_nivel(10,1)
-# mi_enemigo = personaje("vergil", 70, 30, 100, 70)
-# mi_personaje.atacar(mi_enemigo)
-# mi_enemigo.imprimir_atributos()
+michael_jackson = Personaje("Michael Jackon", 20, 15, 10, 100)           
+tlatoani = Guerrero("<<<Apocalipto>>>", 50, 70, 30, 100, 5)
+merlin = Mago("<<<Merlin>>>", 20, 15, 10, 100, 5)
 
+#tlatoani.elegir_arma()
+#merlin.elegir_arma()
 
-#print(mi_personaje.dañar(mi_enemigo))
-#print(mi_personaje.esta_vivo())
-# print ("--------------------")
-# mi_personaje.imprimir_atributos()
+#Imprimir atributos antes de la tragedia
+tlatoani.imprimir_atributos()
+merlin.imprimir_atributos()
+michael_jackson.imprimir_atributos()
 
-# mi_personaje.nombre = "jojo"
-# mi_personaje.fuerza = "30"
-# mi_personaje.vida = "23"
-# mi_personaje.inteligencia = "55"
+#Ataques masivos nucleares 3000
+michael_jackson.atacar(tlatoani)
+tlatoani.atacar(merlin)
+merlin.atacar(michael_jackson)
 
-#constructor de la clase
+#Imprimir atributos despues de la tragedia
+tlatoani.imprimir_atributos()
+merlin.imprimir_atributos()
+michael_jackson.imprimir_atributos()
 
-# print("el nombre del personaje es ", mi_personaje.nombre)
-# print("la fuerza de mi personaje es ", mi_personaje.fuerza)
-# print("la vida de mi personaje es ", mi_personaje.vida)
-# print("la inteligencia de mi personaje es ", mi_personaje.inteligencia)
+# Ejemplo de uso
+mi_personaje = Personaje('DRILLO', 40, 10, 30, 100)
+mi_personaje.imprimir_atributos()
+mi_enemigo = Personaje("Vergil", 70, 30, 70, 100)
+mi_personaje.atacar(mi_enemigo)
